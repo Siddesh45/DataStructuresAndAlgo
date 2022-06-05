691f6bc8b07c6fd3b88c5660a3207832c31cdf4a
Siddesh42 <104648002+Siddesh42@users.noreply.github.com>
2022-06-06 01:02:44 +0530

    Adding Dll And Stack

---
+++ data/structures/linked/list/DoubleLL.java
@@ -0,0 +1,7 @@
+package data.structures.linked.list;
+
+public class DoubleLL {
+    public static void main(String[] args) {
+        System.out.println("This is double linkedList");
+    }
+}
---
+++ data/structures/stack/Stack.java
@@ -0,0 +1,7 @@
+package data.structures.stack;
+
+public class Stack {
+    public static void main(String[] args) {
+        System.out.println("This is Stack");
+    }
+}
